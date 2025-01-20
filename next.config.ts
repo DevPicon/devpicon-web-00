7da4a0540dcd3f793191f49f5aed77c517277c1c
DevPicon <DevPicon@users.noreply.github.com>
2025-01-20 06:35:15 +0000

    deploy: 7022684d70db421c63457c59ac1f4742d5680222

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "export", // Exporta como aplicación estática
-  basePath: "/devpicon-web-00", // Reemplaza <nombre-del-repo> con el nombre de tu repositorio
-  assetPrefix: "/devpicon-web-00/", // Asegúrate de usar el mismo nombre que en basePath
-};
-
-export default nextConfig;
