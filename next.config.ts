4c27f7767e136e2baf3ca5af6cab7ab6cd3a7d5f
Armando Picón <picondev@gmail.com>
2025-01-20 03:07:24 -0300

    update

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export", // Exporta como aplicación estática
-  basePath: "/devpicon", // Reemplaza <nombre-del-repo> con el nombre de tu repositorio
-  assetPrefix: "/devpicon/", // Asegúrate de usar el mismo nombre que en basePath
+  basePath: "/devpicon-web-00", // Reemplaza <nombre-del-repo> con el nombre de tu repositorio
+  assetPrefix: "/devpicon-web-00/", // Asegúrate de usar el mismo nombre que en basePath
 };
 
 export default nextConfig;
